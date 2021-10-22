8a76b1cc29abaa83130242c38b579b6ab0e4d74a
zhscn <zhangsong325@gmail.com>
2021-10-22 17:52:47 +0800

    update research config

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -12,16 +12,30 @@
 (setq org-directory (file-truename "~/org/"))
 
 ;;; bibtex-actions
-(setq bibtex-actions-bibliography (concat org-directory "bibliography/ref.bib"))
-(with-eval-after-load "bibtex-actions"
+(setq bibtex-completion-bibliography `(,(concat org-directory "bibliography/ref.bib"))
+      bibtex-completion-library-path `(,(concat org-directory "pdf/"))
+      bibtex-completion-notes-path (concat org-directory "roam")
+      bibtex-actions-bibliography bibtex-completion-bibliography)
+
+(with-eval-after-load "embark"
+  (setq bibtex-actions-at-point-function 'embark-act)
   (add-to-list 'embark-target-finders 'bibtex-actions-citation-key-at-point)
   (add-to-list 'embark-keymap-alist '(bib-reference . bibtex-actions-map))
   (add-to-list 'embark-keymap-alist '(citation-key . bibtex-actions-buffer-map)))
 (advice-add #'completing-read-multiple :override #'consult-completing-read-multiple)
 
+;;; org-cite
+(autoload #'oc-bibtex-actions "oc-bibtex-actions")
+(setq org-cite-global-bibliography bibtex-actions-bibliography
+      org-cite-insert-processor 'oc-bibtex-actions
+      org-cite-follow-processor 'oc-bibtex-actions
+      org-cite-activate-processor 'oc-bibtex-actions)
+
 ;;; org-roam
 (setq org-roam-v2-ack t
       org-roam-directory (concat org-directory "roam"))
+(with-eval-after-load "org-roam"
+  (org-roam-db-autosync-mode +1))
 
 ;;; org-tree-slide
 (defvar-local org-tree-slide--on nil)
