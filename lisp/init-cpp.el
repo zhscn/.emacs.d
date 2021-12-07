c645fe4a384ac5ad27e997fc297ea03fcb6af343
zhscn <zhangsong325@gmail.com>
2021-12-07 14:57:41 +0800

    enable tree sitter in c-mode

--- lisp/init-cpp.el
+++ lisp/init-cpp.el
@@ -78,6 +78,8 @@
 (unless *is-mac*
   (straight-use-package 'tree-sitter)
   (straight-use-package 'tree-sitter-langs)
+  (add-hook 'c-mode-hook #'tree-sitter-mode)
+  (add-hook 'c-mode-hook #'tree-sitter-hl-mode)
   (add-hook 'c++-mode-hook #'tree-sitter-mode)
   (add-hook 'c++-mode-hook #'tree-sitter-hl-mode))
 
