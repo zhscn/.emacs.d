41984d5bdf6dd5e7a3bb6aeded3d0f34dec7161d
zhscn <zhangsong325@gmail.com>
2022-05-02 20:23:32 +0800

    update tree-sitter

--- lisp/init-cpp.el
+++ lisp/init-cpp.el
@@ -90,12 +90,11 @@
      :cancel-token :eldoc-hover))
   (define-key lsp-mode-map (kbd "C-;") #'lsp-hover-manually))
 
-(unless *is-mac*
-  (straight-use-package 'tree-sitter)
-  (straight-use-package 'tree-sitter-langs)
-  (add-hook 'c-mode-hook #'tree-sitter-mode)
-  (add-hook 'c-mode-hook #'tree-sitter-hl-mode)
-  (add-hook 'c++-mode-hook #'tree-sitter-mode)
-  (add-hook 'c++-mode-hook #'tree-sitter-hl-mode))
+(straight-use-package 'tree-sitter)
+(straight-use-package 'tree-sitter-langs)
+(add-hook 'c-mode-hook #'tree-sitter-mode)
+(add-hook 'c-mode-hook #'tree-sitter-hl-mode)
+(add-hook 'c++-mode-hook #'tree-sitter-mode)
+(add-hook 'c++-mode-hook #'tree-sitter-hl-mode)
 
 (provide 'init-cpp)
