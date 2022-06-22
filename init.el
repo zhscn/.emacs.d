e56559e0563b8dd64ec11656c1912a525dc85479
zhscn <zhangsong325@gmail.com>
2022-06-22 23:53:26 +0800

    update

--- init.el
+++ init.el
@@ -1,4 +1,10 @@
 ;;; -*- lexical-binding: t; -*-
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "theme" user-emacs-directory))
+
+(require 'init-site-lisp)
+(require 'init-builtin)
+(require 'init-package)
 
 (require 'init-complete)
 (require 'init-company)
