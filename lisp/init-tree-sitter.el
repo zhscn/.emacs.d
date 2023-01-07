b2355072a05e3bdaa8a1a9dec9d48b8aef8b6344
zhscn <zhangsong325@gmail.com>
2023-01-07 13:24:58 +0800

    remove external tree sitter

--- lisp/init-tree-sitter.el
+++ lisp/init-tree-sitter.el
@@ -1,10 +1,3 @@
 ;;; -*- lexical-binding: t -*-
-(straight-use-package 'tree-sitter)
-(straight-use-package 'tree-sitter-langs)
-
-(add-hook 'c-mode-hook #'tree-sitter-mode)
-(add-hook 'c-mode-hook #'tree-sitter-hl-mode)
-(add-hook 'c++-mode-hook #'tree-sitter-mode)
-(add-hook 'c++-mode-hook #'tree-sitter-hl-mode)
 
 (provide 'init-tree-sitter)
