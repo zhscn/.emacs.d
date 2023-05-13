01ccb529ff7809627e49aa86567776b50b7da215
zhscn <zhangsong325@gmail.com>
2023-05-13 18:57:56 +0800

    remove copilot

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -5,7 +5,6 @@
 (global-company-mode +1)
 (company-prescient-mode +1)
 (prescient-persist-mode +1)
-(global-copilot-mode)
 
 (keymap-set company-active-map "C-n" #'company-select-next)
 (keymap-set company-active-map "C-p" #'company-select-previous)
@@ -14,9 +13,5 @@
   (delq 'company-clang company-backends)
   ;; disable inline previews
   (delq 'company-preview-if-just-one-frontend company-frontends))
-
-(with-eval-after-load "copilot"
-  (keymap-set copilot-completion-map "<tab>" 'copilot-accept-completion)
-  (keymap-set copilot-completion-map "TAB" 'copilot-accept-completion))
 
 (provide 'init-company)
