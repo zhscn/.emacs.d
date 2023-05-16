8eb48ec0ab44b173e2cf09f78286863d4b9f9906
zhscn <zhangsong325@gmail.com>
2023-05-16 14:22:46 +0800

    remove prescient

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -3,8 +3,6 @@
 (setq company-minimum-prefix-length 1)
 
 (global-company-mode +1)
-(company-prescient-mode +1)
-(prescient-persist-mode +1)
 
 (keymap-set company-active-map "C-n" #'company-select-next)
 (keymap-set company-active-map "C-p" #'company-select-previous)
