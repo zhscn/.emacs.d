0da1c51c46fdee86440c0fa58f481f01f7969290
zs <zhangsong325@gmail.com>
2025-08-21 00:11:37 +0800

    move load package to early-init.el

--- early-init.el
+++ early-init.el
@@ -1,3 +1,11 @@
-(setq byte-compile-warnings '(cl-functions))
-(setq package-enable-at-startup nil)
+(setq byte-compile-warnings '(cl-functions)
+      package-enable-at-startup nil)
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "theme" user-emacs-directory))
+
+(require 'init-site-lisp)
+(require 'init-builtin)
+(require 'init-package)
+
 (provide 'early-init)
