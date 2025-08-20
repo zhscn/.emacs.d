1c19bd509731e4996554aa90b1d4a59f3a8762ba
zs <zhangsong325@gmail.com>
2025-08-21 00:11:39 +0800

    remove unnecessary keybindings

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -4,9 +4,6 @@
 
 (global-company-mode +1)
 
-(keymap-set company-active-map "C-n" #'company-select-next)
-(keymap-set company-active-map "C-p" #'company-select-previous)
-
 (with-eval-after-load 'company
   (delq 'company-clang company-backends))
 
