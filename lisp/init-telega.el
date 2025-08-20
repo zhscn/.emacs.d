273a751ccda575b55560f7d2ca60c0ecf28b3f46
zs <zhangsong325@gmail.com>
2025-08-21 00:11:38 +0800

    update telega

--- lisp/init-telega.el
+++ lisp/init-telega.el
@@ -3,6 +3,12 @@
 
 (setq telega-use-images t
       telega-open-file-function 'org-open-file
+      telega-filter-button-width '(0.25 8 17)
+      telega-filters-custom
+      '(("Main" . main)
+        ("Groups" . (type basicgroup supergroup))
+        ("Channels" . (type channel))
+        ("Archive" . archive))
       telega-proxies '((:server "localhost" :port 1080 :enable t :type (:@type "proxyTypeHttp"))))
 
 (defun +telega-open-file (file)
@@ -20,6 +26,9 @@
   (set-face-attribute 'mytelega-face nil :font (font-spec :family "Sarasa Fixed SC"))
   (setq buffer-face-mode-face 'mytelega-face)
   (make-variable-buffer-local 'face-font-rescale-alist)
+  (add-to-list 'face-font-rescale-alist '("-Noto Sans-" . 0.6))
+  (add-to-list 'face-font-rescale-alist '("-Noto Sans Math-" . 0.5))
+  (add-to-list 'face-font-rescale-alist '("-Noto Color Emoji-" . 0.7))
   (add-to-list 'face-font-rescale-alist '("-Noto Sans Georgian-" . 0.8))
   (add-to-list 'face-font-rescale-alist '("-Symbola-" . 0.8))
   (buffer-face-mode +1))
