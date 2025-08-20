4c7852b9f0b9e378f1725c1f5258a52175eb3c95
zs <zhangsong325@gmail.com>
2025-08-21 00:11:38 +0800

    update

--- early-init.el
+++ early-init.el
@@ -1,11 +1,5 @@
 (setq byte-compile-warnings '(cl-functions)
       package-enable-at-startup nil)
 
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-(add-to-list 'load-path (expand-file-name "theme" user-emacs-directory))
-
-(require 'init-site-lisp)
-(require 'init-builtin)
-(require 'init-package)
 
 (provide 'early-init)
