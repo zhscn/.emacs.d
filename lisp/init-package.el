c7561959f98435b6d43bf6a2268ce249ccc9ac26
zs <zhangsong325@gmail.com>
2025-08-21 00:11:40 +0800

    remove nix-mode

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -47,7 +47,6 @@
   (elpaca hl-todo)
   (elpaca minions)
   (elpaca persp-mode)
-  (elpaca nix-mode)
   (elpaca (vertico :files (:defaults "extensions/*")))
   (elpaca marginalia)
   (elpaca orderless)
