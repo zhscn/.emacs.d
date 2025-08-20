04f55bd9ce746e0131eab35e91a8554fbfe188ab
zs <zhangsong325@gmail.com>
2025-08-21 00:11:37 +0800

    wip update research config

--- lisp/init-complete.el
+++ lisp/init-complete.el
@@ -3,6 +3,8 @@
 (straight-use-package 'marginalia)
 (straight-use-package 'orderless)
 (straight-use-package 'consult)
+(straight-use-package 'embark)
+(straight-use-package 'embark-consult)
 
 ;; vertico
 (add-hook 'after-init-hook #'vertico-mode)
@@ -47,5 +49,21 @@
 (define-key global-map [remap project-find-regexp] #'consult-ripgrep)
 (with-eval-after-load "org-mode"
   (define-key org-mode-map [remap consult-imenu] #'consult-org-heading))
+
+;; embark
+(define-key minibuffer-mode-map (kbd "C-.") #'embark-act)
+(define-key minibuffer-mode-map (kbd "C-'") #'embark-dwim)
+(define-key minibuffer-mode-map (kbd "M-h") #'embark-bindings)
+
+(setq prefix-help-command #'embark-prefix-help-command)
+
+(add-to-list 'display-buffer-alist
+             '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+               nil
+               (window-parameters (mode-line-format . none))))
+
+;; embark-consult
+(with-eval-after-load "embark"
+  (add-hook 'embark-collect-mode-hook #'consult-preview-at-point-mode))
 
 (provide 'init-complete)
