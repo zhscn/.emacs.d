45a5487095c0a9e69650c206f08f62d4e6807755
zs <zhangsong325@gmail.com>
2025-08-21 00:11:39 +0800

    add compilot.el

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -7,5 +7,10 @@
 (keymap-set company-active-map "C-n" #'company-select-next)
 (keymap-set company-active-map "C-p" #'company-select-previous)
 (setq company-backends (delete 'company-clang company-backends))
+(with-eval-after-load 'company
+  ;; disable inline previews
+  (delq 'company-preview-if-just-one-frontend company-frontends))
 
+(define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
+(define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
 (provide 'init-company)
