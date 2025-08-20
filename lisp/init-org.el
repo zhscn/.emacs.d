0985d2f791bbcd4cb77f9432f8bac69e2885a92c
zs <zhangsong325@gmail.com>
2025-08-21 00:11:37 +0800

    update org

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -8,21 +8,23 @@
 (straight-use-package 'darkroom)
 (straight-use-package 'org-roam-bibtex)
 
-;;; ivy-bibtex
-(autoload 'ivy-bibtex "ivy-bibtex" nil t)
+(setq org-directory (file-truename "~/org/"))
 
+;;; ivy-bibtex
 (setq ivy-re-builders-alist
       '((ivy-bibtex . ivy--regex-ignore-order)
         (t . ivy--regex-plus)))
 
-(setq bibtex-completion-bibliography '("~/Documents/test.bib"))
+(setq bibtex-completion-bibliography `(,(concat org-directory "bibliography/ref.bib")))
+;; (setq bibtex-completion-library-path '(""))
 (setq bibtex-completion-pdf-field "File")
-(setq bibtex-completion-notes-path "~/Documents/notes")
+(setq bibtex-completion-notes-path (concat org-directory "notes"))
 
 ;;; org-ref
-(setq reftex-default-bibliography '("~/Documents/test.bib"))
-(setq org-ref-pdf-directory "File")
-(setq org-ref-bibliography-notes "~/Documents/notes")
+(setq org-ref-completion-library 'org-ref-ivy-cite)
+(setq reftex-default-bibliography `(,(concat org-directory "bibliography/ref.bib")))
+(setq org-ref-pdf-directory (file-truename "~/Zotero/storage"))
+(setq org-ref-bibliography-notes (concat org-directory "note.org"))
 
 ;;; org-roam
 (autoload 'org-roam-node-find "org-roam" nil t)
@@ -31,6 +33,7 @@
 (setq org-agenda-text-search-extra-files (directory-files-recursively org-roam-directory "\\.org$"))
 
 ;;; org-roam-bibtex
+(add-hook 'org-roam-mode-hook #'org-roam-bibtex-mode)
 (with-eval-after-load "org-roam"
   (require 'org-ref)
   (require 'org-roam-bibtex))
