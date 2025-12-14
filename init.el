f3e44a6dadf62b56f3dc5dad80520fc41d4e3383
zs <zhangsong325@gmail.com>
2025-12-14 13:02:38 +0800

    remove lsp-bridge

--- init.el
+++ init.el
@@ -21,27 +21,8 @@
 (require 'init-lisp)
 (require 'init-coq)
 (require 'init-haskell)
-
-(defun load-lsp-bridge ()
-  (add-to-list 'load-path (expand-file-name "lsp-bridge" user-emacs-directory))
-  (require 'lsp-bridge)
-  (setq lsp-bridge-c-lsp-server "ccls")
-  (add-hook 'after-init-hook #'global-lsp-bridge-mode)
-  (keymap-set global-map "M-." #'lsp-bridge-find-def)
-  (keymap-set global-map "M-," #'lsp-bridge-find-def-return)
-  (keymap-set global-map "M-?" #'lsp-bridge-find-references)
-  (keymap-set global-map "M-'" #'lsp-bridge-code-action)
-  (keymap-set global-map "M-[" #'lsp-bridge-code-format))
-
-(defun load-lsp ()
-  (require 'init-company)
-  (require 'init-lsp))
-
-(defvar use-lsp-bridge nil)
-
-(if use-lsp-bridge
-    (load-lsp-bridge)
-  (load-lsp))
+(require 'init-company)
+(require 'init-lsp)
 
 (when (file-exists-p custom-file) (load custom-file))
 (put 'dired-find-alternate-file 'disabled nil)
